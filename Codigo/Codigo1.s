2e81aa1df363e81f4f958be746dfc61125bad68a
Victorio Giovanni Valdez Medina <95736584+EducaTec01@users.noreply.github.com>
2023-11-01 13:04:49 -0700

    Update Codigo1.s
    Codigo de Examen

--- Codigo/Codigo1.s
+++ Codigo/Codigo1.s
@@ -1,50 +1,66 @@
 @  
-@  Title: Practica 0 Templete para programa ARM Assembly   
-@  Filename: hello.s
-@  Autor: ____Aqui su nombre___
-@  Date: ______
-@  Description : Hola Mundo en ARM Assembly
+@  Title: Examen recueperacion U2  
+@  Filename: Codigo1.s
+@  Autor: Victorio Giovanni Valdez Medina
+@  Date: 01-11-2023
+@  Description :  programa que encuentre la suma de todos los enteros del 2 al 2000
 @  Imput: -
-@  Ouput: HOLA MUNDO to console
+@  Ouput: La suma de los enteros de 2 a 2000 es:
 @  
 @------------------------------------
 @   HIGH LEVEL LANGUAGE
 @------------------------------------
-@ Esta sección es donde se declaran y se inicializan las variables y los datos que se usarán en el programa.
-@
-@  // Primer programa de ejemplo en C++ solución
-@  
-@  #include <iostream>
-@  int main () 
-@  {
-@      std::cout << "Hola, mundo";
-@      return 0;
-@  }
-@  
+@    #include <stdio.h>
+@    
+@    int main() {
+@        int sum = 0;
+@        for (int i = 2; i <= 2000; i++) {
+@            sum += i;
+@        }
+@        printf("La suma de los enteros de 2 a 2000 es: %d\n", sum);
+@        return 0;
+@   }
 
-
-@ hello.s programa usa los recursos básicos a compilarlo
-@ Esto es un comentario que describe el propósito general del programa.
+@ Codigo1.s es un programa que se encanrga de calcular la suma de los enteros del 2 al 2000
 
 @------------------------------------
 @   DATA SECTION   
 @------------------------------------
 @ Esta sección es donde se declaran y se inicializan las variables y los datos que se usarán en el programa.
 
-.section	.data   @ Comienza la sección de datos. Aquí se pueden declarar variables.
-.section	.text   @ Comienza la sección de texto. Aquí es donde se coloca el código ejecutable del programa.
-.globl		_start   @ Esta línea hace que la etiqueta _start sea visible fuera de este archivo, lo que permite que el enlazador la encuentre.
+.data
+    str_format:
+        .asciz "La suma de los enteros de 2 a 2000 es: %d\n"
+    .balign 4
+sum: 
+    .word 0
+
+.text
+.global _start
 
 @------------------------------------
 @   CODE SECTION   
 @------------------------------------
 @ Esta sección contiene el código ejecutable del programa.
 
-_start:		   @ Esta es la etiqueta que marca el punto de entrada del programa.
-nop          @ no operation, Instrucción de No Operación (NOP). No hace nada y se utiliza a menudo para fines de depuración o para introducir retrasos controlados.
-mov r7, $1   @ exit syscall; Mueve el valor 1 (que representa la llamada al sistema de salida) al registro r7.
-svc $0		   @ wake kernel.  Invoca una llamada al sistema (System Call). Esto causa una interrupción que transfiere el control al kernel del sistema operativo para que realice la operación solicitada en el registro r7.
-.end         @ Marca el fin del archivo fuente. Los resultados se ven enconsk
+_start:
+    mov r1, #2          @ Inicializamos r1 con 2 (el primer número)
+    mov r2, #2000       @ Establecemos r2 con 2000 (límite superior)
+    mov r0, #0          @ Inicializamos r0 para almacenar la suma
+
+loop:
+    add r0, r0, r1      @ Sumamos r0 con el valor actual de r1
+    add r1, r1, #1      @ Incrementamos r1 en 1
+ 
+    cmp r1, r2          @ Comparamos r1 con r2
+    ble loop            @ Si r1 <= r2, saltamos de nuevo a 'loop'
+
+    ldr r1, =str_format @ Cargamos la dirección de la cadena de formato en r1
+    mov r2, r0          @ Movemos el valor de la suma a r2
+    bl printf           @ Llamamos a la función printf
+
+    mov r7, #1          @ Llamamos a la función de salida (exit)
+    swi 0
 
 @-------------------------------------
 @   MAKEFILE  
@@ -52,40 +68,32 @@ svc $0		   @ wake kernel.  Invoca una llamada al sistema (System Call). Esto cau
 @  Esta sección compila y conecta visual Studio Code con la extensión CMAKE o manualmente en powerShell, Linux Terminal
 @  NOTA: makefile es un lenguaje y le gustan los TABS (no espacion en identaciones)
 @
-@  # Define el archivo fuente en ensamblador  
-@  SRC = templete.s  
+@  # Makefile para compilar el programa en ensamblador ARM
 @  
-@  # Define el nombre del archivo de salida  
-@  OUT = templete  
+@  # Compilador y enlazador
+@  AS = as
+@  LD = ld
 @  
-@  # Define el ensamblador y las banderas  
-@  AS = as  
-@  ASFLAGS = -g  
+@  # Opciones del compilador
+@  ASFLAGS = -g
+@  LDFLAGS = -e _start
 @  
-@  # Define el enlazador y las banderas  
-@  LD = ld  
-@  LDFLAGS =  
+@  # Nombre del archivo de salida
+@  OUTPUT = suma_enteros
 @  
-@  # Define el GCC o G++ segun corresponda  
-@  GCC = gcc  
-@  GCCFLAGS =  -g  
+@  # Objetivos por defecto
+@  all: $(OUTPUT)
 @  
-@  # Corrida de make  
-@  all: $(OUT)  
+@  # Regla para ensamblar el código
+@  $(OUTPUT): suma_enteros.o
+@      $(LD) $(LDFLAGS) -o $@ $^
 @  
-@  $(OUT): $(SRC)  
-@          $(AS) $(ASFLAGS) $(SRC) -o $(OUT).o  
-@          $(GCC) $(LDFLAGS) $(OUT).o -o $(OUT)  
-@  # Borra temporales y ejecutable  
-@  clean:  
-@          rm -f $(OUT) $(OUT).o  
-@  # Corrida  
-@  run: $(OUT)  
-@          ./$(OUT)  
+@  # Regla para ensamblar el código fuente
+@  suma_enteros.o: suma_enteros.s
+@      $(AS) $(ASFLAGS) -o $@ $<
 @  
-@  # Entrar al Debugger  
-@
-@
-@  Basado en el templete: https://github.com/tectijuana/interfaz/edit/master/templete/readme.md
-@  gef: $(OUT)  
-@          gdb  $(OUT)  
+@  # Regla para limpiar archivos generados
+@  clean:
+@      rm -f $(OUTPUT) suma_enteros.o
+@  
+@  .PHONY: all clean
